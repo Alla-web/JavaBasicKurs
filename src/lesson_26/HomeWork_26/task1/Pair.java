41dbe7c212dd20b57262de04c1776d5829c05c0a
Alla-Nazarenko <umnyj.start@gmail.com>
2024-10-17 16:55:37 +0200

    HomeWork 26

---
+++ src/lesson_26/HomeWork_26/task1/Pair.java
@@ -0,0 +1,40 @@
+package lesson_26.HomeWork_26.task1;
+
+public class Pair<T> {
+
+    private T first;
+    private T second;
+
+    //constructor
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    //getters
+    public T getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    //setters
+    public void setFirst(T value) {
+        this.first = value;
+    }
+
+    public void setSecond(T value) {
+        this.second = value;
+    }
+
+    //methods
+    public void swap() {
+        T temp = first;
+        first = second;
+        second = temp;
+    }
+
+
+}
---
+++ src/lesson_26/HomeWork_26/task1/PairApp.java
@@ -0,0 +1,42 @@
+package lesson_26.HomeWork_26.task1;
+
+public class PairApp {
+    public static void main(String[] args) {
+        System.out.println("---------------------------Pair<Integer>-----------------------------");
+        System.out.println("----------------------Cоздали переменные--------------------------------");
+        Pair<Integer> integerPair = new Pair<>(1, 2);
+        System.out.println("First = " + integerPair.getFirst() +
+                "; " + "Second = " + integerPair.getSecond() + ".");
+
+        System.out.println("----------------------Переназначили переменные--------------------------------");
+        integerPair.setFirst(3);
+        integerPair.setSecond(4);
+        System.out.println("First = " + integerPair.getFirst() +
+                "; " + "Second = " + integerPair.getSecond() + ".");
+
+        System.out.println("----------------------Обменяли значения переменных--------------------------------");
+        integerPair.swap();
+        System.out.println("First = " + integerPair.getFirst() +
+                "; " + "Second = " + integerPair.getSecond() + ".");
+
+        System.out.println("---------------------------Pair<String>-----------------------------");
+        Pair<String> stringPair = new Pair<>("Hello", "World");
+        System.out.println("First = " + stringPair.getFirst());
+        System.out.println("Second = " + stringPair.getSecond());
+        stringPair.swap();
+        System.out.println("First = " + stringPair.getFirst());
+        System.out.println("Second = " + stringPair.getSecond());
+
+        System.out.println("---------------------------Pair<Character>-----------------------------");
+        Pair<Character> characterPair = new Pair<>('A', 'L');
+        System.out.println("First = " + characterPair.getFirst());
+        System.out.println("Second = " + characterPair.getSecond());
+        characterPair.swap();
+        System.out.println("First = " + characterPair.getFirst());
+        System.out.println("Second = " + characterPair.getSecond());
+
+
+    }
+
+
+}
