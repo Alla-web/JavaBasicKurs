584c050769cc6d1200623ad67546d6a986d71e6d
Alla-Nazarenko <umnyj.start@gmail.com>
2024-10-21 07:51:09 +0200

    HomeWork 27

--- src/lesson_27/Person.java
+++ src/lesson_27/Person.java
@@ -11,7 +11,7 @@ public class Person {
     //constructor
     public Person(String email, String password) {
         setEmail(email); // поменяли на вызов сэттера
-        this.password = password;
+        setPassword(password);
     }
 
     //getters and setters
@@ -31,7 +31,9 @@ public class Person {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        if (isPasswordValid(password)) {
+            this.password = password;
+        }
     }
 
     //methods
@@ -43,56 +45,91 @@ public class Person {
                 '}';
     }
 
-    //
     private boolean isEmailValid(String email) {
         /*
         test_1@gmail.com - пример нормального имейла
             схема проверки:
-        1) должен содержать символ @ и только один
-        2) должен содержать символ . и после символа @
-        3) после последней точки есть 2 и более символа
-        4) содержит алфавит; цифры; -; _; .; @
-        5) перед символом @ должен быть хотя бы один символ
-        6) первый символ должен быть буквой
+        1) email должен содержать символ @ и только один
+        2) email должен содержать символ . и после символа @
+        3) в email после последней точки есть 2 и более символа
+        4) email содержит алфавит; цифры; -; _; .; @
+        5) в email перед символом @ должен быть хотя бы один символ
+        6) в email первый символ должен быть буквой
          */
+        if (email != null || !email.isEmpty()) {
+            // 1) email должен содержать символ @ и только один
+            // находими индекс первого символа @ в строке
+            int indexAt = email.indexOf('@');
+            // находим последний индекс последнего символа @ в строке
+            //int lastAt = email.lastIndexOf('@');
+            if (indexAt == -1 || indexAt != email.lastIndexOf('@')) return false;
 
-        // 1) должен содержать символ @ и только один
-        int indexAt = email.indexOf('@');
-        //int lastAt = email.lastIndexOf('@');
-        if (indexAt == -1 || indexAt != email.lastIndexOf('@')) return false;
+            // 2) email должен содержать символ . и после символа @
+            int dotIndexAfterAt = email.indexOf('@', indexAt + 1);
+            if (dotIndexAfterAt == -1) return false;
 
-        // 2) должен содержать символ . и после символа @
-        int dotIndexAfterAt = email.indexOf('@', indexAt + 1);
-        if (dotIndexAfterAt == -1) return false;
+            // 3) в email после последней точки есть 2 и более символа
+            int indexLastDot = email.lastIndexOf('.');
+            if (indexLastDot == -1 || indexLastDot == email.length() - 1) return false;
+            if ((email.length() - 1 - indexLastDot) >= 2) return true;
 
+            //  4) email содержит алфавит; цифры; -; _; .; @
+            // алфавит
+            for (int i = 0; i < email.length(); i++) {
+                char currentChar = email.charAt(i);
+                if (Character.isAlphabetic(currentChar) || Character.isDigit(currentChar) || currentChar == '-' || currentChar == '_' || currentChar == '.' || currentChar == '@') {
+                    return true;
+                }
+            }
 
+            // 5) в email перед символом @ должен быть хотя бы один символ
+            if (indexAt > 0) return true;
 
+            // 6) в email первый символ должен быть буквой
+            if (Character.isLetter(email.charAt(0))) return true;
+        }
+        return false;
 
-
-        Character.isAlphabetic('c');
-        //возвращает true, если символ буква
-Character.isDigit(1);
-        //возвращает true, если символ цифра
-        Character.isLetter('D');
-        Character.isLetterOrDigit('-');
-
-
-
-
-
-        return true;
     }
 
-/*
-1) длина >=8
-2) должна быть мин 1 цифра
-3) должна быть мин 1 маленькая буква
-4) должна быть мин 1 большая буква
-5) должен быть мин 1 спецсимвол из набора: ! @ % $ * () [] . ? _
+    private boolean isPasswordValid(String password) {
+    /*
+    1) длина >=8
+    2) должна быть мин 1 цифра
+    3) должна быть мин 1 маленькая буква
+    4) должна быть мин 1 большая буква
+    5) должен быть мин 1 спецсимвол из набора: ! @ % $ * () [] . ? _
 
-создать 5 boolean-переменных и каждая отвечает за свой пункт,
-пароль будет подходить, только если все 5 true
-return b1 && b2 && b3 && b4 && b5;
+    создать 5 boolean-переменных и каждая отвечает за свой пункт,
+    пароль будет подходить, только если все 5 true
+    return b1 && b2 && b3 && b4 && b5;
+    */
+        // 1) длина >=8
+        if (password.length() >= 8) return true;
 
- */
+        // 2) должна быть мин 1 цифра
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) return true;
+        }
+
+        // 3) должна быть мин 1 маленькая буква
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isLowerCase(password.charAt(i))) return true;
+        }
+
+        // 4) должна быть мин 1 большая буква
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isUpperCase(password.charAt(i))) return true;
+        }
+
+        // 5) должен быть мин 1 спецсимвол из набора: ! @ % $ * () [] . ? _
+        for (int i = 0; i < password.length(); i++) {
+            char currentChar2 = password.charAt(i);
+            if (currentChar2 == '!' || currentChar2 == '@' || currentChar2 == '%' || currentChar2 == '$' || currentChar2 == '*' || currentChar2 == '(' || currentChar2 == ')' || currentChar2 == '[' || currentChar2 == ']' || currentChar2 == '.' || currentChar2 == '?' || currentChar2 == '_')
+                return true;
+        }
+        return false;
+    }
+
+
 }
--- src/lesson_27/PersonApp.java
+++ src/lesson_27/PersonApp.java
@@ -6,24 +6,24 @@ public class PersonApp {
         System.out.println(person);
 
         System.out.println("-----------------------Character-------------------------");
-        System.out.println(Character.isLetterOrDigit('@'));
-        System.out.println(Character.isLetterOrDigit('1'));
-        System.out.println(Character.isLetterOrDigit('.'));
-        System.out.println(Character.isLetterOrDigit('F'));
-        System.out.println(Character.isLetterOrDigit('ы'));
-        System.out.println(Character.isAlphabetic('R'));
+        /*
+        System.out.println("isLetter('f') -> " + Character.isLetter('f'));
+        System.out.println("isDigit('5') -> " + Character.isDigit('5'));
+        System.out.println("isLetterOrDigit('-') -> " + Character.isLetterOrDigit('-'));
+        System.out.println("isWhitespace(' ') -> " + Character.isWhitespace(' '));
+        System.out.println("isUpperCase('F') -> " + Character.isUpperCase('F'));
+        System.out.println("isLowerCase('A') -> " + Character.isLowerCase('A'));
+        System.out.println("toUpperCase('a') -> " + Character.toUpperCase('a'));
+        System.out.println("toLowerCase('F') - > " + Character.toLowerCase('F'));
+        Character.isAlphabetic('c');
+        //возвращает true, если символ буква
 
+         */
 
+        System.out.println("person.getEmail().isEmpty() -> " + person.getEmail().isEmpty());
 
-
-
-
-
-
-
-
-
-
+        System.out.println("-----------------------isEmailValid-------------------------");
+        Person person1 =
 
 
 
