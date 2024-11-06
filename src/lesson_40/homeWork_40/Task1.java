970cda828ece5b9111b8a2b5338a121f1258423b
Alla-Nazarenko <umnyj.start@gmail.com>
2024-11-06 19:52:59 +0100

    homework 40

--- src/lesson_40/homeWork_40/Task1.java
+++ src/lesson_40/homeWork_40/Task1.java
@@ -1,5 +1,33 @@
 package lesson_40.homeWork_40;
 
-public class Task1 {
+import java.lang.reflect.Array;
+import java.util.*;
 
-}
+public class Task1 {
+    /*
+    Напишите метод, принимающий строку и возвращающий Список ее слов без повторений,
+    отсортированный в порядке увеличения длин слов.
+    Если строки имеют одинаковую длину - сортировать в естественном порядке
+     */
+    public static void main(String[] args) {
+        String string = "Тестовая строка для удаления слов, которые повторяются. \\\"строка\\\" для удаления!";
+        getUniqueSortedWords(string);
+
+    }// main
+
+    public static List<String> getUniqueSortedWords(String string) {
+        System.out.println("string -> " + string);
+
+        String[] words = string.replaceAll("[^a-zA-Zа-яА-Я\\s]", "").split(" ");
+
+        Set<String> setList = new LinkedHashSet<>(Arrays.asList(words));
+        System.out.println(setList);
+
+        List<String> list1 = new ArrayList<>(setList); //преобразуем сэт для сортировки
+        list1.sort(Comparator.comparingInt(String::length).thenComparing(Comparator.naturalOrder())); //сортируем сэт
+        System.out.println(list1);
+
+        return list1;
+    }
+
+}//class
